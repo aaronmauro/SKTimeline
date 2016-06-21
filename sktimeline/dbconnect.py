70555cee6a57a8f20566da22517ae206c184cbdb
Jared Wenerd <wenerd87@gmail.com>
2016-06-21 10:46:16 -0400

    proper db connect config value for dev

--- sktimeline/dbconnect.py
+++ sktimeline/dbconnect.py
@@ -6,7 +6,7 @@ def connection():
     conn = MySQLdb.connect(host = app.config['DB_HOST'],
                           user = app.config['DB_USER'],
                           passwd = app.config['DB_PASSWORD'],
-                          db = app.config['DB_PASSWORD'])
+                          db = app.config['DB_NAME'])
     c = conn.cursor()
 
     return c, conn
