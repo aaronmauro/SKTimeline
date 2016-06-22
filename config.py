a8e482978686505af58033c8ae0bdb95dbb5129c
Jared Wenerd <wenerd87@gmail.com>
2016-06-22 11:46:32 -0400

    Add session config options default to allow Flask sessions

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ MAIL_PORT=465,
 MAIL_USE_SSL=True,
 MAIL_USERNAME = 'baaronmauro@gmail.com',
 MAIL_PASSWORD = ''
+# MAIL_SUPPRESS_SEND = True # put in instance/config.py for local development
 
 # TODO: replace these
 DB_HOST='localhost'
@@ -16,3 +17,6 @@ DB_PASSWORD='pennstateerie4'
 DB_NAME='sktimeline'
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/sktimeline'
+
+SESSION_TYPE = 'filesystem'
+SECRET_KEY = 'jiboo' # change in instance config for production
